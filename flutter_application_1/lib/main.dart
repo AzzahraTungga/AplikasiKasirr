1dc58a9a6c6dd5260500698e223f33f6ba1b6b30
zea <azzahra_dewi_33rpl@student.smktelkom-mlg.sch.id>
2026-01-28 11:12:15 +0700

    zea

--- flutter_application_1/lib/main.dart
+++ flutter_application_1/lib/main.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'views/RegisterUserView.dart';
-import 'views/dashboard.dart' as dashboard_view;
-import 'views/loginViews.dart' hide DashboardView;
+import 'views/loginViews.dart';
 
 void main() {
   runApp(const MyApp());
@@ -16,8 +15,7 @@ class MyApp extends StatelessWidget {
       initialRoute: '/',
       routes: {
         '/': (context) => const RegisterUserView(),
-        '/login': (context) => const LoginView(),
-        '/dashboard': (context) => const dashboard_view.DashboardView(),
+        '/login': (context) => const LoginView()
       }, 
     );
   }
